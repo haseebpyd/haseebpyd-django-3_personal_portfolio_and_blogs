be466d77e2ef9371939809f346a613d82f89eac2
haseebpyd <haseebzemail@gmail.com>
2020-07-07 16:08:21 +0500

    first commit

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(default="")
+    date = models.DateField()    
+
+    def __str__(self):
+        return self.title
+
+
